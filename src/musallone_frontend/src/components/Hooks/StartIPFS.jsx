28e56ea1ceb7f5fd3e735190808583ef46dd1d4a
fleeB1 <fowler.lee8@gmail.com>
2022-12-29 00:26:17 +0100

    ipfs file storage for videos

---
+++ src/musallone_frontend/src/components/Hooks/StartIPFS.jsx
@@ -0,0 +1,16 @@
+// import  {create}  from 'ipfs-core';
+
+// export function startIpfsActor(){
+//     let ipfs = null;
+//     async function makeIPFS() {
+//         try {
+//             ipfs = await create();
+//             console.log("IPFS created")
+//         } catch (error) {
+//             console.log("failed to create IPFS")
+//             ipfs = null
+//         }
+//         return ipfs;
+//     }
+//     let ipfs = await makeIPFS()
+// }
--- src/musallone_frontend/src/components/Pages/SimpleForm.jsx
+++ src/musallone_frontend/src/components/Pages/SimpleForm.jsx
@@ -5,6 +5,9 @@ import Paper from '@mui/material/Paper';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import { CardHeader, Container, Grid, Stack, Typography } from '@mui/material';
+import { Web3Storage } from 'web3.storage'
+import  {create}  from 'ipfs-core';
+
 
 
 export default function SimpleForm() {
@@ -14,6 +17,56 @@ export default function SimpleForm() {
   const [message, setMessage] = React.useState('');
   const [numbContracts, setNumbContracts] = React.useState(0);
   const [allContracts, setAllContracts] = React.useState([]);
+
+  async function ipfsActor(file) {
+    let ipfs = await create();
+    console.log("ipfs: ", ipfs)
+    const data = 'Hello, <jack>'
+    const results = await ipfs.add(data)
+    console.log("results:" , results)
+    // let string = await ipfs.cat(results.path)
+    console.log("returned data:", ipfs.cat(results.path))
+
+    // for (const { cid } of results) {
+    //   // CID (Content IDentifier) uniquely addresses the data
+    //   // and can be used to get it again.
+    //   console.log(cid.toString())
+    // }
+  }
+
+//   async function makeAndStoreFiles () {
+//       // You can create File objects from a Blob of binary data
+//       // see: https://developer.mozilla.org/en-US/docs/Web/API/Blob
+//       // Here we're just storing a JSON object, but you can store images,
+//       // audio, or whatever you want!
+
+//       let client = GetWebStorageClient();
+   
+//       const obj = { hello: 'world' }
+//       const blob = new Blob([JSON.stringify(obj)], { type: 'application/json' })
+    
+//       const files = [
+//         new File(['contents-of-file-1'], 'plain-utf8.txt'),
+//         new File([blob], 'hello.json')
+//       ]
+
+//       let cid = await client.put(files);
+//       console.log('stored files with cid:', cid);
+//   }
+
+//   function GetWebStorageClient () {
+//     // If you're just testing, you can paste in a token
+//     // and uncomment the following line:
+//     // return 'paste-your-token-here'
+  
+//     // In a real app, it's better to read an access token from an
+//     // environement variable or other configuration that's kept outside of
+//     // your code base. For this to work, you need to set the
+//     // WEB3STORAGE_TOKEN environment variable before you run your code.
+//     let token = process.env.WEB3STORAGE_TOKEN;
+//     console.log("token: ", token)
+//   return new Web3Storage({ token: token })
+// }
 
   async function doContract() {
     const greeting = await musallone_backend.greet(name)
@@ -69,6 +122,13 @@ export default function SimpleForm() {
       noValidate
       autoComplete="off"
     > 
+
+        {/* <input
+          type="file"
+          onChange={makeAndStoreFiles} /> */}
+
+      <Button variant="contained" onClick={ipfsActor}> Upload data </Button>
+
       <TextField id="filled-basic" label="Contract name" variant="filled" onChange={(ev) => setName(ev.target.value)}/>
       <TextField id="filled-basic" label="Contract details" variant="filled" multiline
           rows={8} onChange={(ev) => setDetails(ev.target.value)}/>
